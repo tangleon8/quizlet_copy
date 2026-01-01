ae28bb4d24ae9f1cd75c30a3840d7816b0b4fe5f
Leon <leontang27@icloud.com>
2026-01-01 15:04:18 -0500

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -204,7 +204,7 @@ function AppContent() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1>Quizlet</h1>
+          <h1>Questly</h1>
         </header>
         <main className="App-main">
           <Auth />
